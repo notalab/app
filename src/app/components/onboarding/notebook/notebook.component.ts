fc3b3c45140423cd074b0403d9d04939e691bdff
mtchdev <fsmitchm@gmail.com>
2020-08-05 12:48:57 +0800

    Create new notebook on submit

--- src/app/components/onboarding/notebook/notebook.component.ts
+++ src/app/components/onboarding/notebook/notebook.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { AuthService } from 'app/components/auth/auth.service';
 import { Router } from '@angular/router';
 import { Notebook } from 'app/models/core/Notebook';
+import { NotebookService } from 'app/components/app/notebook.service';
 import * as cl from 'color';
 
 type Step = 'notebook' | 'color' | 'finish';
@@ -18,13 +19,14 @@ export class OnboardingNotebookComponent implements OnInit {
         notes: []
     };
     public step: Step = 'notebook';
+    public notebookLoading = false;
 
     public errors = {
         notebook: null
     };
     public notebookTitle: string;
 
-    constructor(public authService: AuthService, private router: Router) { }
+    constructor(public authService: AuthService, private router: Router, private notebookService: NotebookService) { }
 
     ngOnInit() {
     }
@@ -34,9 +36,11 @@ export class OnboardingNotebookComponent implements OnInit {
 
         if (!this.notebookTitle) {
             this.errors.notebook = 'Please enter a notebook title.';
+            return;
         }
-        if (this.notebookTitle && this.notebookTitle.length > 30) {
-            this.errors.notebook = 'The title must be under 30 characters.';
+        if (this.notebookTitle.length > 24) {
+            this.errors.notebook = 'The title must be under 24 characters.';
+            return;
         }
 
         if (this.errors.notebook) { return; }
@@ -46,11 +50,34 @@ export class OnboardingNotebookComponent implements OnInit {
     }
 
     public setColor(): void {
+        if (!this.notebook.color) {
+            this.errors.notebook = 'Please select a color.';
+            return;
+        }
+
         this.step = 'finish';
     }
 
+    public selectColor(color: string): void {
+        this.errors.notebook = null;
+        this.notebook.color = color;
+    }
+
     public lighten(color: string): string {
-        return cl(color).lighten(0.75).hex();
+        return cl('#' + color).lighten(0.75).hex();
+    }
+
+    public finish(): void {
+        this.notebookLoading = true;
+
+        this.notebookService.createNotebook(this.notebook).subscribe(
+            data => {
+                this.notebookService.notebooks.push(data.data);
+                this.notebook = undefined;
+                this.notebookLoading = false;
+                this.router.navigate(['app/inbox']);
+            }
+        );
     }
 
 }
